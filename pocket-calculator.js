c6ea4cdf3bb1a49f4d8c0ab30c2f6f846cc46adc
Siddharth Lal <senseiwooo@gmail.com>
2019-01-08 13:19:34 -0500

    formatted css

---
+++ pocket-calculator.js
@@ -0,0 +1,71 @@
+function button0() {
+
+}
+
+function button1() {
+
+}
+
+function button2() {
+
+}
+
+function button3() {
+
+}
+
+function button4() {
+
+}
+
+function button5() {
+
+}
+
+function button6() {
+
+}
+
+function button7() {
+
+}
+
+function button8() {
+
+}
+
+function button9() {
+
+}
+
+function decimal() {
+
+}
+
+function clear() {
+
+}
+
+function negate() {
+
+}
+
+function percent() {
+
+}
+
+function divide() {
+
+}
+
+function multiply() {
+
+}
+
+function subtract() {
+
+}
+
+function equal() { 
+
+}
