4105e993f9a4ad7f5ba143edb7f9c679e2f3ef64
Siddharth Lal <senseiwooo@gmail.com>
2019-01-10 13:20:44 -0500

    idk

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,45 +1,80 @@
-function button0() {
+var answer;
 
+function button0() {
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "0";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button1() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "1";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button2() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "2";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button3() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "3";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button4() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "4";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button5() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "5";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button6() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "6";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button7() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "7";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button8() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "8";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function button9() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += "9";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function decimal() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += ".";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function clear() {
@@ -55,17 +90,29 @@ function percent() {
 }
 
 function divide() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += " / ";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function multiply() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += " x ";
+  answer = document.getElementById("result").innerHTML;
 }
 
 function subtract() {
-
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += " - ";
+  answer = document.getElementById("result").innerHTML;
 }
 
-function equal() { 
-
+function equal() {
+  answer = document.getElementById("result").innerHTML;
+  answer = answer.toString();
+  document.getElementById("result").textContent += " = ";
+  answer = document.getElementById("result").innerHTML;
 }
